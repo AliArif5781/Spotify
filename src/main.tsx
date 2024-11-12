14705ae1ba413af5e6586f5f06f1e3225e910794
AliArif5781 <aliairf92@gmail.com>
2024-11-12 19:18:25 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./style/main.scss";
-
+import { Provider } from "react-redux";
+import store from "./app/store.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
